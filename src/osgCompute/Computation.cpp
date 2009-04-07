f6e9692c95830fceb7bb21fe104aa28192f55ef0
maikKeller <maikKeller@48f291c0-f584-499c-a92c-3b0c12dbe5aa>
2009-04-07 11:04:42 +0000

    -Do not traverse all (SVT Group)

--- src/osgCompute/Computation.cpp
+++ src/osgCompute/Computation.cpp
@@ -66,13 +66,14 @@ namespace osgCompute
             else if( nv.getVisitorType() == osg::NodeVisitor::UPDATE_VISITOR )
             {
                 update( nv );
+                traverse( nv );
             }
             else if( nv.getVisitorType() == osg::NodeVisitor::EVENT_VISITOR )
             {
                 handleevent( nv );
+                traverse( nv );
             }
 
-            traverse( nv );
 
             nv.popFromNodePath(); 
         } 
